dfe7120d1a887ab1048381a6b899686c39907358
GDcheerios <minecreeper0913@gmail.com>
2025-06-04 07:40:48 -0500

    remove plugins.lua

--- lua/config/plugins.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { "numToStr/Comment.nvim", opts = {}, lazy = false }, -- comment toggle
-  { "nyoom-engineering/nyoom.nvim", opts = {}, lazy = false } -- theme
-}
-
-require('Comment').setup()
