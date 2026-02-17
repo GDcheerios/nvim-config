4975a26413bbf4f1fa70a03b7e6f9a60f349ff85
Brayden Messerschmidt <minecreeper0913@gmail.com>
2026-02-16 18:15:48 -0600

    fix keymap nil

--- init.lua
+++ init.lua
@@ -9,6 +9,11 @@ vim.opt.foldmethod = "marker"
 vim.opt.foldmarker = "# region,# endregion"
 vim.opt.foldlevel = 99
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+
 -- colorscheme
 vim.opt.termguicolors = true
 vim.api.nvim_create_autocmd("User", {
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,8 @@
 local builtin = require('telescope.builtin')
 
+-- keymap syntax
+-- (mode, lhs (keys), rhs (action), opts)
+
 -- basic keymaps
 vim.g.mapleader = '\\'
 vim.g.maplocalleader = '\\'
@@ -11,26 +14,35 @@ vim.keymap.set('n', '<leader>l', '<cmd>Lazy<CR>', { desc = '[L]azy Plugin Manage
 vim.keymap.set('n', '<leader>m', '<cmd>Mason<CR>', { desc = '[M]ason Manager' })
 
 -- lsp keybinds
-local on_attach = function(client, bufnr)
+local function safe_on_attach(client, bufnr)
+  if not bufnr or bufnr == 0 then
+    bufnr = vim.api.nvim_get_current_buf()
+  end
 
-  -- Buffer-local keymaps (only active when an LSP is attached to the buffer)
+  local bufopts = { buffer = bufnr, silent = true }
+
   -- Find
-  vim.keymap.set('n', '<leader>fd', vim.lsp.buf.definition, { buffer = bufnr, desc = "[F]ind [D]efinition" })
-  vim.keymap.set('n', '<leader>fq', vim.lsp.buf.hover, { buffer = bufnr, desc = "[F]ind [Q]uote" })
-  vim.keymap.set('n', '<leader>fs', vim.lsp.buf.signature_help, { buffer = bufnr, desc = "[F]ind [S]ignature" })
-  vim.keymap.set('n', '<leader>fi', vim.lsp.buf.declaration, { buffer = bufnr, desc = "[F]ind [I]nit" })
+  vim.keymap.set('n', '<leader>fd', function() vim.lsp.buf.definition() end, vim.tbl_extend('force', bufopts, { desc = "[F]ind [D]efinition" }))
+  vim.keymap.set('n', '<leader>fq', function() vim.lsp.buf.hover() end, vim.tbl_extend('force', bufopts, { desc = "[F]ind [Q]uote" }))
+  vim.keymap.set('n', '<leader>fs', function() vim.lsp.buf.signature_help() end, vim.tbl_extend('force', bufopts, { desc = "[F]ind [S]ignature" }))
+
+  if client.server_capabilities.declarationProvider then
+    vim.keymap.set('n', '<leader>fi', function() vim.lsp.buf.declaration() end, vim.tbl_extend('force', bufopts, { desc = "[F]ind [I]nit" }))
+  end
 
   -- Refactor
-  vim.keymap.set('n', '<leader>rr', vim.lsp.buf.rename, { buffer = bufnr, desc = "[R]efactor [R]ename" })
-  vim.keymap.set('n', '<leader>rf', function() vim.lsp.buf.format { async = true } end, { buffer = bufnr, desc = "[R]efactor [F]ormat" })
+  vim.keymap.set('n', '<leader>rr', function() vim.lsp.buf.rename() end, vim.tbl_extend('force', bufopts, { desc = "[R]efactor [R]ename", noremap = true }))
+  vim.keymap.set('n', '<leader>rf', function() vim.lsp.buf.format { async = true } end, vim.tbl_extend('force', bufopts, { desc = "[R]efactor [F]ormat" }))
 
-  -- Code Action
-  vim.keymap.set('n', '<leader><space>', vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
+  -- Code action (guard if needed)
+  if client.server_capabilities.codeActionProvider then
+    vim.keymap.set('n', '<leader><space>', function() vim.lsp.buf.code_action() end, vim.tbl_extend('force', bufopts, { desc = "Code Action" }))
+  end
 
   -- Diagnostics
-  vim.keymap.set('n', '<leader>d[', vim.diagnostic.goto_prev, { buffer = bufnr, desc = "Go to previous diagnostic" })
-  vim.keymap.set('n', '<leader>d]', vim.diagnostic.goto_next, { buffer = bufnr, desc = "Go to next diagnostic" })
-  vim.keymap.set('n', '<leader>d<space>', vim.diagnostic.set_loclist, { buffer = bufnr, desc = "Open location list with diagnostics" })
+  vim.keymap.set('n', '<leader>d[', function() vim.diagnostic.goto_prev() end, vim.tbl_extend('force', bufopts, { desc = "Go to previous diagnostic" }))
+  vim.keymap.set('n', '<leader>d]', function() vim.diagnostic.goto_next() end, vim.tbl_extend('force', bufopts, { desc = "Go to next diagnostic" }))
+  vim.keymap.set('n', '<leader>d<space>', function() vim.diagnostic.setloclist() end, vim.tbl_extend('force', bufopts, { desc = "Open location list with diagnostics" }))
 end
 
 -- telescope
@@ -40,15 +52,17 @@ vim.keymap.set('n', '<leader>sb', builtin.buffers,    { desc = '[S]earch [B]uffe
 vim.keymap.set('n', '<leader>sh', builtin.help_tags,  { desc = '[S]earch [H]elp' })
 
 -- line comment toggle
-vim.keymap.set('n', '<C-_>', '<Plug>(comment_toggle_linewise_current)', opts)
-vim.keymap.set('v', '<C-_>', '<Plug>(comment_toggle_linewise_visual)', opts)
+vim.keymap.set('n', '<C-_>', '<Plug>(comment_toggle_linewise_current)', { silent = true })
+vim.keymap.set('v', '<C-_>', '<Plug>(comment_toggle_linewise_visual)', { silent = true })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('my.lsp.attach', { clear = true }),
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client then
-            on_attach(client, args.buf)
-        end
+  group = vim.api.nvim_create_augroup('my.lsp.attach', { clear = true }),
+  callback = function(args)
+    local bufnr = args.buf or vim.api.nvim_get_current_buf()
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and bufnr then
+      safe_on_attach(client, bufnr)
     end
+  end,
 })
+
